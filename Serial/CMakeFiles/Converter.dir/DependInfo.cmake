08b8e29e18c5d43982b49091d66f903ebf6204d9
jcluck <jcluck@jcluckpc.(none)>
2011-10-08 17:52:59 -0400

    Added Serial Package which currently publishes ultrasonic data

---
+++ Serial/CMakeFiles/Converter.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jcluck/ros_workspace/ycp-robot/Serial/src/Converter.cpp" "/home/jcluck/ros_workspace/ycp-robot/Serial/CMakeFiles/Converter.dir/src/Converter.o"
+  "/home/jcluck/ros_workspace/ycp-robot/Serial/src/DrRobotMotionSensorDriver.cpp" "/home/jcluck/ros_workspace/ycp-robot/Serial/CMakeFiles/Converter.dir/src/DrRobotMotionSensorDriver.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "BOOST_CB_DISABLE_DEBUG"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
