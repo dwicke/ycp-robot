560d21c3003706d720d846633d5f984f46eb28d7
Jason <jcluck@ycp.edu>
2011-12-11 15:51:04 -0500

    Sensor Fixes

--- Serial/src/Converter.cpp
+++ Serial/src/Converter.cpp
@@ -8,11 +8,8 @@
 #include "DrRobotMotionSensorDriver.hpp"
 
 //serial parameters
-//TODO Check to make sure this is the right port on the beagleboard
 #define PORT "/dev/ttyUSB0"
 #define BAUD 115200
-
-#define RIGHT_MOTOR_OFFSET 18
 
 //prototypes
 float IR_Convert(int IRValue);
@@ -34,15 +31,26 @@ long microtime(void);
 int count = 0;
 int countmax = 0;
 long motor_time = 0;
-
 using namespace DrRobot_MotionSensorDriver;
 
 
 //callback for the motor Data
 //if there is new information, it needs to be converted and sent to the robot
 //motors are controlled by taking in a velocity value which is then set for the specific motor channel
+void motorCallback(const robot_msgs::MotorData::ConstPtr& msg){
+        ROS_INFO("Motor data received: \n");
+        ROS_INFO("motor1: %lf", msg->motor_left_velocity);
+        ROS_INFO("motor2: %lf", msg->motor_left_velocity);
+	//send left/right values to the robot
+	int motorPASS = driver->sendMotorCtrlAllCmd(Velocity, Motor_Convert(msg->motor_left_velocity), Motor_Convert(-1*msg->motor_right_velocity), 0,0,0,0,100);
+       	if(motorPASS < 0) ROS_INFO("Motor data was not sent to the Robot!");
+}
 
-float lleft=-100,lright=-100;
+//callback for the motor Data
+//if there is new information, it needs to be converted and sent to the robot
+//motors are controlled by taking in a velocity value which is then set for the specific motor channel
+
+/*float lleft=-100,lright=-100;
 void motorcmd(float left,float right)
 {
         //Sending repeat commands seems so sometimes cause the robot to stop- avoid doing that
@@ -68,7 +76,7 @@ void motorCallback(const robot_msgs::MotorData::ConstPtr& msg){
         motor_time=microtime()+msg->motor_left_time;
         
         return;
-}
+}*/
 
 void serialInit(){
         //initialize internal vars to default - constructor
@@ -118,41 +126,31 @@ int main(int argc, char**argv){
                 //set the sensor data
 
                 //Ultrasonic: data is 8 bit unsigned, in cm's
-                SensorMsg.ultrasonic_frontLeft_distance = rangeSensorData->usRangeSensor[0];
-                SensorMsg.ultrasonic_frontCenter_distance = rangeSensorData->usRangeSensor[1];
-                SensorMsg.ultrasonic_frontRight_distance = rangeSensorData->usRangeSensor[2];
-                SensorMsg.ultrasonic_rearRight_distance = rangeSensorData->usRangeSensor[3];
-                SensorMsg.ultrasonic_rearCenter_distance = rangeSensorData->usRangeSensor[4];
-                SensorMsg.ultrasonic_rearLeft_distance = rangeSensorData->usRangeSensor[5];
+                //SensorMsg.ultrasonic_frontLeft_distance = rangeSensorData->usRangeSensor[0];
+                //SensorMsg.ultrasonic_frontCenter_distance = rangeSensorData->usRangeSensor[1];
+                //SensorMsg.ultrasonic_frontRight_distance = rangeSensorData->usRangeSensor[2];
+                SensorMsg.ultrasonic_frontRight_distance = rangeSensorData->usRangeSensor[3];
+                SensorMsg.ultrasonic_frontCenter_distance = rangeSensorData->usRangeSensor[4];
+                SensorMsg.ultrasonic_frontLeft_distance = rangeSensorData->usRangeSensor[5];
                 
                 //Infrared: data is floats
                 SensorMsg.infrared_frontLeftLeft_distance = IR_Convert(rangeSensorData->irRangeSensor[0]);
-                SensorMsg.infrared_frontLeftCenter_distance = IR_Convert(rangeSensorData->irRangeSensor[1]);
-                SensorMsg.infrared_frontRightCenter_distance = IR_Convert(rangeSensorData->irRangeSensor[2]);
-                SensorMsg.infrared_frontRightRight_distance = IR_Convert(rangeSensorData->irRangeSensor[3]);
-                SensorMsg.infrared_right_distance = IR_Convert(rangeSensorData->irRangeSensor[4]);
-                SensorMsg.infrared_rear_distance = IR_Convert(rangeSensorData->irRangeSensor[5]);
-                SensorMsg.infrared_left_distance = IR_Convert(rangeSensorData->irRangeSensor[6]);
+                SensorMsg.infrared_frontLeftCenter_distance = IR_Convert(customSensorData->customADData[2]);
+                SensorMsg.infrared_frontRightCenter_distance = IR_Convert(customSensorData->customADData[3]);
+                SensorMsg.infrared_frontRightRight_distance = IR_Convert(customSensorData->customADData[4]);
+                SensorMsg.infrared_right_distance = IR_Convert(customSensorData->customADData[5]);
+                SensorMsg.infrared_rear_distance = IR_Convert(customSensorData->customADData[6]);
+                SensorMsg.infrared_left_distance = IR_Convert(customSensorData->customADData[7]);
 
                 //human sensor, returned from robot in a different message
                 SensorMsg.human_left_motion = standardSensorData->humanSensorData[0];
                 SensorMsg.human_left_presence = standardSensorData->humanSensorData[1];
                 SensorMsg.human_right_motion = standardSensorData->humanSensorData[2];
                 SensorMsg.human_right_presence = standardSensorData->humanSensorData[3];
-                        
+                
+		//print sensor data for debug        
 		print_ultrasonic();
-		
-		
-		
-		//print infrareds, 1 is stored in the range sensor, other is in custom data
-		ROS_INFO("Infrared range sensor 1 = %f\n",  IR_Convert(rangeSensorData->irRangeSensor[0]));
-        	ROS_INFO("Infrared range sensor 2 = %f\n",  IR_Convert(customSensorData->customADData[2]));
-  	     	ROS_INFO("Infrared range sensor 3 = %f\n",  IR_Convert(customSensorData->customADData[3]));
-		ROS_INFO("Infrared range sensor 4 = %f\n",  IR_Convert(customSensorData->customADData[4]));
-        	ROS_INFO("Infrared range sensor 5 = %f\n",  IR_Convert(customSensorData->customADData[5]));
-  	     	ROS_INFO("Infrared range sensor 6 = %f\n",  IR_Convert(customSensorData->customADData[6]));
-		ROS_INFO("Infrared range sensor 7 = %f\n",  IR_Convert(customSensorData->customADData[7]));
-
+		print_infrared();
 
                 //now publish the sensor data
                 sensordata_pub.publish(SensorMsg);
@@ -167,24 +165,25 @@ int main(int argc, char**argv){
         return 0;
 }
 void print_infrared(){
-        //print out the sensor data messages for testing purposes
-        //ROS_INFO("Infrared range sensor 1 = %d\n",  SensorMsg.infrared_frontLeftLeft_distance);
-        //ROS_INFO("Infrared range sensor 2 = %d\n", SensorMsg.infrared_frontLeftCenter_distance);
-        //ROS_INFO("Infrared range sensor 3 = %d\n",  SensorMsg.infrared_frontRightCenter_distance);
-        ROS_INFO("Infrared range sensor 4 = %d\n", (rangeSensorData->irRangeSensor[3]));
-        ROS_INFO("Infrared range sensor 5 = %d\n", (rangeSensorData->irRangeSensor[4]));
-        ROS_INFO("Infrared range sensor 6 = %d\n", (rangeSensorData->irRangeSensor[5]));
-        ROS_INFO("Infrared range sensor 7 = %d\n", (rangeSensorData->irRangeSensor[6]));
+        //print infrareds, 1 is stored in the range sensor, other is in custom data
+	ROS_INFO("Infrared range sensor 1 = %f\n",  IR_Convert(rangeSensorData->irRangeSensor[0]));
+	ROS_INFO("Infrared range sensor 2 = %f\n",  IR_Convert(customSensorData->customADData[2]));
+     	ROS_INFO("Infrared range sensor 3 = %f\n",  IR_Convert(customSensorData->customADData[3]));
+	ROS_INFO("Infrared range sensor 4 = %f\n",  IR_Convert(customSensorData->customADData[4]));
+	ROS_INFO("Infrared range sensor 5 = %f\n",  IR_Convert(customSensorData->customADData[5]));
+     	ROS_INFO("Infrared range sensor 6 = %f\n",  IR_Convert(customSensorData->customADData[6]));
+	ROS_INFO("Infrared range sensor 7 = %f\n",  IR_Convert(customSensorData->customADData[7]));
 }
 void print_ultrasonic(){
         //print out the sensor data messages for testing purposes
         //ROS_INFO("Ultrasonic range sensor 1 = %d\n", (rangeSensorData->usRangeSensor[0]));
         //ROS_INFO("Ultrasonic range sensor 2 = %d\n", (rangeSensorData->usRangeSensor[1]));
         //ROS_INFO("Ultrasonic range sensor 3 = %d\n", (rangeSensorData->usRangeSensor[2]));
+	
 	//sensors were changed around due to bad sensor / control board issues at YCP
-        ROS_INFO("Ultrasonic range sensor 1 = %d\n", (rangeSensorData->usRangeSensor[3]));
-        ROS_INFO("Ultrasonic range sensor 2 = %d\n", (rangeSensorData->usRangeSensor[4]));
-        ROS_INFO("Ultrasonic range sensor 3 = %d\n", (rangeSensorData->usRangeSensor[5]));
+        ROS_INFO("Ultrasonic right = %d\n", (rangeSensorData->usRangeSensor[3]));
+        ROS_INFO("Ultrasonic center = %d\n", (rangeSensorData->usRangeSensor[4]));
+        ROS_INFO("Ultrasonic left = %d\n", (rangeSensorData->usRangeSensor[5]));
 }
 
 /*Convert the IR Sensor to a value in cm.  Note that this is currently not too accurate,
