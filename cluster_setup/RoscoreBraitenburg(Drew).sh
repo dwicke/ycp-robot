0c008d49e0e7b98254c7b0b252004ee2e37c9577
cboyle1 <cboyle1@ycp.edu>
2011-12-12 13:53:47 -0500

    Fix port for cluster scripts.

--- cluster_setup/RoscoreBraitenburg(Drew).sh
+++ cluster_setup/RoscoreBraitenburg(Drew).sh
@@ -2,7 +2,7 @@ echo Configuring this machine for the Roscore / Braitenburg setup.
 echo
 
 echo Setting ROS parameters...
-export ROS_MASTER_URI=http://192.168.2.201
+export ROS_MASTER_URI=http://192.168.2.201:11311
 export ROS_IP=192.168.2.201
 echo Using $ROS_IP for this machine and it will connect to $ROS_MASTER_URI as server.
 echo
